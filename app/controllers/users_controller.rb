a9f14c2250a97f680d45db15df5cbd2fba6fcc16
JerYoMat <jeremy.emata@gmail.com>
2019-01-29 18:33:06 -0500

    refactored form to use form_for, started implementing strong_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :create]
   
   def new
     @user = User.new 
@@ -9,9 +9,22 @@ class UsersController < ApplicationController
     set_user
   end 
 
+  def create 
+    #some stuff
+    if @user.save 
+    else 
+      render 'new'
+    end 
+  end 
+
 
 
 private 
+
+def user_params
+  params.require(:user).permit(:name, :email, :password,
+                               :password_confirmation, :smoker)
+end
 
   def set_user
     @user = User.find(params[:id])
