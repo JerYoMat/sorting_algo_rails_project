9c9272fda93868ab1e20ee673d24eb966935c47e
JerYoMat <jeremy.emata@gmail.com>
2019-01-29 16:37:39 -0500

    remigrated db, added some basic user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup #setup method automaticall is run before each test 
+  @user = User.new(name: "UserTest Name", email: "user@usertestcase.com")
+ end 
+
+ test "should be valid" do 
+   assert @user.valid?   
+ end
+ 
+ test "name should be present" do 
+   @user.name = "   "
+   assert_not @user.valid?   
+ end 
 end
