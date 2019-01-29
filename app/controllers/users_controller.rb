f19eb6358fa096472eb3fba3a7f14f29e12d04d7
JerYoMat <jeremy.emata@gmail.com>
2019-01-29 17:44:44 -0500

    added a single test user to seed file, implemented show view and added styling to display as sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  
   def new
   end
-end
+
+  def show 
+    set_user
+  end 
+
+
+
+private 
+
+  def set_user
+    @user = User.find(params[:id])
+  end  
+end 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "test1", email: "test1@gmail.com", password: "1234567")
