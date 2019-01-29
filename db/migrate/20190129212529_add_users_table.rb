02b562792259ab137f06339c458037cfdbcd464c
JerYoMat <jeremy.emata@gmail.com>
2019-01-29 16:27:32 -0500

    added users migration

---
+++ db/migrate/20190129212529_add_users_table.rb
@@ -0,0 +1,11 @@
+class AddUsersTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name 
+      t.string :password_digest
+      t.string :email
+      t.string :status 
+      t.timestamps 
+    end 
+  end
+end
