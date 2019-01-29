78cecc387ca31c9a6fe26417f4b0d675658d53a3
JerYoMat <jeremy.emata@gmail.com>
2019-01-28 23:05:26 -0500

    refactor - cleaned up layout with another partial, simplified tests, simplified styling and refactored routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,20 +1,16 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
   
   #Added by generating the controller and makes sure that a 200 status code is returned when the home url is visited
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Home" #assert_select tests for the presence of a specific html tag
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About"
   end
