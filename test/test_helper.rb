a5f99a7947dcc65b6a792217f1aa7ad8edeebeb9
JerYoMat <jeremy.emata@gmail.com>
2019-01-28 23:18:08 -0500

    format

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,6 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper  #by including this we can now test for the right title by relying on previous work. Not currently in use.
   # Add more helper methods to be used by all tests here...
 end
