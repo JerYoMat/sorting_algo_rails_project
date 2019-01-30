bd776e145fd9b0c0e120a55ffdfd4034c4f782ae
JerYoMat <jeremy.emata@gmail.com>
2019-01-30 18:43:31 -0500

    added validations, tests passing

--- app/models/lesson_topic.rb
+++ app/models/lesson_topic.rb
@@ -1,4 +1,10 @@
 class LessonTopic < ApplicationRecord
+  #ASSOCIATIONS
   has_many :tips 
   has_many :users, through: :tips 
+
+  #VALIDATIONS
+  validates :name, presence: true 
+  validates :description, presence: true 
+  validates :course_order, presence: true 
 end
