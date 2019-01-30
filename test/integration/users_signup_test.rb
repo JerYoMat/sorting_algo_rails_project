ca9480cd713ba0caea71f03743ecdff4147623b1
JerYoMat <jeremy.emata@gmail.com>
2019-01-29 19:16:31 -0500

    added post signup route, updated signup integration test to assert that correct error message is displayed

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div ul li[1]', "Name can't be blank"
   end
 end
