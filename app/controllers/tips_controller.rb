51d176cc0af8931ec81c07b7b9687241a6f92fa9
JerYoMat <jeremy.emata@gmail.com>
2019-01-31 05:14:21 -0500

    added tip show view

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,13 +1,20 @@
 class TipsController < ApplicationController
 before_action :logged_in_user, only:[:new, :create, :edit, :update, :destroy]
 before_action :correct_user, only: [:create, :edit, :update, :destroy] 
-before_action :Set_tip, only: [:create, :edit, :update, :show, :destroy]
-def new
+before_action :set_tip, only: [:create, :edit, :update, :show, :destroy]
+  def new
     @tip = Tip.new 
   end 
 
   def create 
-
+    @tip = current_user.tips.build(tip_params)
+    if @tip.save
+      flash[:success] = "Thanks! Your tip has been added."
+      redirect_to root_path
+    else
+      @options = LessonTopic.all.map{|l| [l.name, l.id]}    
+      render 'static_pages/home'
+    end
   end 
 
   def edit 
@@ -20,7 +27,6 @@ def new
   end  
 
   def destroy 
-
      @tip.destroy
   end 
 
