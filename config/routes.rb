5ff5c735997e409467715349161501f05dac5c1c
JerYoMat <jeremy.emata@gmail.com>
2019-01-30 21:16:48 -0500

    added resources for other models, updated fixtures to automatically associate to others for tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users
-  resources :tips 
+  resources :tips, only: [:create, :edit, :update, :destroy, :show]
+  resources :lessons, only: [:show, :index]
 end
